16e88308d90818ef07397805e137e2c11f45bd18
Avani <avani.jadeja@gmail.com>
2023-03-11 15:15:28 -0600

    button click code on .js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,84 @@
+$(function() {});
 
+// today is the current day
+var today = moment().format("dddd, MMMM Do");
 
+// now is the current hour
+var now = moment().format("H A");
 
-$(function () {
+// planWorkday entries for each hour of the workday
+var planWorkday = [
+	{ time: "9 AM", event: "" },
+	{ time: "10 AM", event: "" },
+	{ time: "11 AM", event: "" },
+	{ time: "12 PM", event: "" },
+	{ time: "1 PM", event: "" },
+	{ time: "2 PM", event: "" },
+	{ time: "3 PM", event: "" },
+	{ time: "4 PM", event: "" },
+	{ time: "5 PM", event: "" }
+];
 
+// adds local storage to checkPrevious var, and if needed updates planWorkday
+var checkPrevious = JSON.parse(localStorage.getItem("dayPlanner"));
+	if (checkPrevious !== null) {
+	planWorkday = checkPrevious;
+}
+// displays the current day
+$("#currentDay").text(today);
 
-    //Display current date on Header of the page.so using moment function() its fomat like Saturday, March 11th 
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
-    
-    //create planWorkDay array which store 9 to 5 hours 
-    var planWorkDay =[
-      { time : "9 AM", event: "" },
-      { time : "10 AM", event: "" },
-      { time : "11 AM", event: "" },
-      { time : "12 AM", event: "" },
-      { time : "1 PM", event: "" },
-      { time : "2 PM", event: "" },
-      { time : "3 PM", event: "" },
-      { time : "4 PM", event: "" },
-      { time : "5 PM", event: "" },
-    ];
+// creates the color for the row
+planWorkday.forEach(function(timeBlock, index) {
+	var timeLabel = timeBlock.time;
+	var blockColor = colorMe(timeLabel);
 
-    planWorkDay.forEach(function(timeBlock, index){
-      var timeLabel = timeBlock.time;
-      var blockColor = colorMe(timeLabel);
-      var row =
-      '<div class="time-block" id = "' + 
-       index + 
-      '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' + 
-      timeLabel +
-      '</div><textarea class="form-control' + 
-      blockColor +
-      '">' +
-      timeBlock.event +
-      '</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-plus"></i></button></div></div></div>';
-        
-     $("container-lg").append(row);
-      
-    });
+	// variable containing the entire row, including the button
+	var row =
+		'<div class="time-block" id="' +
+		index +
+		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+		timeLabel +
+		'</div><textarea class="form-control ' +
+		blockColor +
+		'">' +
+		timeBlock.event +
+		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-plus"></i></button></div></div></div>';
 
-    function colorMe(time)
-    {
-      var planNow = moment(now,"H A");
-      var planEntry = moment(time, "H A");
+	// show entry rows
+	$(".container-fluid").append(row);
+});
 
-      if (planNow.isBefore(planEntry) === true)
-      {
-        return "future";
-      }
-      else if (planNow.isAfter(planEntry) === true)
-      {
-        return "past";
-      }
-      else 
-      {
-        return "present";
-      }
-    }
+// function for past, present, next color
+function colorMe(time) {
+	var planNow = moment(now, "H A");
+	var planEntry = moment(time, "H A");
+	if (planNow.isBefore(planEntry) === true) {
+		return "future";
+	} else if (planNow.isAfter(planEntry) === true) {
+		return "past";
+	} else {
+		return "present";
+	}
+}
 
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    
+// save button
+$(".saveBtn").on("click", function(event) {
+	var blockID = parseInt(
+		$(this)
+			.closest(".time-block")
+			.attr("id")
+	);
+	
+	var userEntry = $.trim(
+		$(this)
+			.parent()
+			.siblings("textarea")
+			.val()
+	);
 
-    
+	// saves the entry
+	planWorkday[blockID].event = userEntry;
 
-  });
-  
+	// updates planWorkday local storage
+	localStorage.setItem("dayPlanner", JSON.stringify(planWorkday));
+});
